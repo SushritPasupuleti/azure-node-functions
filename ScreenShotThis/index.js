351d65061b5c3b1d07364569b52960fb7276d2ef
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-03-31 14:29:43 +0530

    feat: add basic screenshotting pipeline with puppeteer

--- ScreenShotThis/index.js
+++ ScreenShotThis/index.js
@@ -1,13 +1,58 @@
+const puppeteer = require("puppeteer");
+const wait = require("waait");
+
+const cached = new Map();
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('👀 JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    const qs = new URLSearchParams(req.query);
+    console.log(`👋 {you:url-to-screenshot}?${qs.toString()}`);
+    const screenshotBuffer = await getScreenshot(
+        `{you:url-to-screenshot}?${qs.toString()}`
+    );
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: screenshotBuffer,
+        headers: {
+            "content-type": "image/jpeg",
+        },
     };
+};
+
+async function getScreenshot(url) {
+    // first check if this value has been cached
+    const cachedImage = cached.get(url);
+
+    if (cachedImage) {
+        // return cachedImage;
+    }
+
+    const browser = await puppeteer.launch({
+        //pass options like font
+        defaultViewport: null,
+        captureBeyondViewport: false,
+        args: [
+            '--start-maximized',
+        ],
+        // headless: true,
+    });
+    const page = await browser.newPage();
+
+    // await page.setViewport({ width: 1200, height: 630 });
+    await page.goto(url);
+    await page.emulateMediaFeatures([
+        { name: 'prefers-color-scheme', value: 'dark' },
+    ])
+
+    await wait(1000);
+
+    const buffer = await page.screenshot({
+        fullPage: true,
+    });
+    cached.set(url, buffer);
+
+    await page.close();
+    await browser.close();
+
+    return buffer;
 }
